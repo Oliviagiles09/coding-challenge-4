15bdb23f45c951c3e09750e3ef5608103304c398
Oliviagiles09 <giles11@usf.edu>
2025-02-02 21:47:01 -0500

    Task 2 - Sales Report

--- cc_4.js
+++ cc_4.js
@@ -9,3 +9,13 @@ if (purchaseAmount > 100) {
 let finalAmount = purchaseAmount - (purchaseAmount * discount);
 
 consol.log('Final amount after discount: $${finalAmount.toFixed(2)}');
+
+// Task 2
+let sales = [120, 85, 200, 150, 90];
+
+let totalSales = 0;
+for (let i = 0; i < sales.length; i++) {
+  totalSales += sales [i];
+}
+
+consol.log('Total sales: $${totalSales}');
